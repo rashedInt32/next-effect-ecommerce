01a378b39485e08eac695f80916308bbb002d5af
rashed parvez <parvez08eee@gmail.com>
2026-04-09 09:29:25 +0600

    Add routes

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -1,3 +1,27 @@
+import { PgLive } from "@/src/infrastructure/database";
+import { ProductService } from "@/src/services/ProductService";
+import { Effect } from "effect";
+import { NextResponse } from "next/server";
+
 export async function GET() {
-  return Response.json({ products: [] });
+  const program = Effect.gen(function* () {
+    const service = yield* ProductService;
+    const products = yield* service.getAll();
+    return yield* Effect.succeed(NextResponse.json({ products }));
+  }).pipe(
+    Effect.catchAll((error) => {
+      return Effect.succeed(
+        NextResponse.json({ error: error.message }, { status: 500 }),
+      );
+    }),
+    Effect.withSpan("GET /api/products"),
+  );
+
+  const products = await Effect.runPromise(
+    program.pipe(
+      Effect.provide(ProductService.Default),
+      Effect.provide(PgLive),
+    ),
+  );
+  return products;
 }
